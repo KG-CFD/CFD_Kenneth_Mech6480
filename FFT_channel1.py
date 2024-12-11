7b6555a5898221a96630de220337552126f80ee2
KG-CFD <kennethgoodall96@gmail.com>
2024-12-11 12:57:35 +1000

    FFT with corrected horizontal axis scale(KHz)

--- FFT_channel1.py
+++ FFT_channel1.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 # Read the Excel file
 file_path = "scope.csv"
 data = pd.read_csv(file_path)
-# Display the first few rows
+
 print(data.head())
 
-# Extract time and voltage
+
 time = data['second']
 voltage = data['Volt1']
 
@@ -17,7 +17,7 @@ voltage = data['Volt1']
 v_max = 0.1056
 v_min =0.0129
 
-phase_shift = np.arcsin(((voltage - v_min ) / ((v_max - v_min) / 2)-1))
+phase_shift = np.arcsin(((voltage - v_min ) / ((v_max - v_min) / 2))-1)
 
 # Print the phase shift values
 print("First few phase shift values:", phase_shift.head())
@@ -27,10 +27,10 @@ print("First few phase shift values:", phase_shift.head())
 
 sampling_frequency = 1 / (time[1] - time[0])
 print(sampling_frequency)
-# Perform FFT on the phase shift data
+
 fft_phase_shift = np.fft.fft(phase_shift)
 
-# Calculate the magnitude of the FFT (this gives the amplitude spectrum)
+
 fft_magnitude = np.abs(fft_phase_shift)
 
 # Calculate the Power Spectral Density
@@ -43,7 +43,7 @@ frequencies = np.fft.fftfreq(len(phase_shift), d=(time[1] - time[0]))
 positive_frequencies = frequencies[:len(frequencies) // 2]
 psd_positive = psd[:len(frequencies) // 2]
 
-# Plot the Power Spectral Density
+# Plot the PSD
 
 plt.figure(figsize=(10, 6))
 plt.semilogy(positive_frequencies / 10000, psd_positive)
