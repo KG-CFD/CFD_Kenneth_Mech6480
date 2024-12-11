44a01dff53f991dd69e047933c863f2a5652b7fc
KG-CFD <kennethgoodall96@gmail.com>
2024-12-11 12:47:07 +1000

    FFT with corrected horizontal axis scale(KHz)

--- FFT_channel1.py
+++ FFT_channel1.py
@@ -1,20 +1,18 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fftpack import fft, fftfreq
+
 
 # Read the Excel file
-file_path = "scope.csv"  # Replace with your file name
-data = pd.read_csv(file_path)  # Reads the entire Excel sheet
-
+file_path = "scope.csv"
+data = pd.read_csv(file_path)
 # Display the first few rows
 print(data.head())
 
 # Extract time and voltage
 time = data['second']
 voltage = data['Volt1']
-print("First few time values:", time.head())  # Time data
-print("First few voltage values:", voltage.head())  # Voltage data
+
 
 v_max = 0.1056
 v_min =0.0129
@@ -26,9 +24,9 @@ print("First few phase shift values:", phase_shift.head())
 
 
 
-# Sampling frequency (assuming the time data is evenly spaced)
-sampling_frequency = 1 / (time[1] - time[0])
 
+sampling_frequency = 1 / (time[1] - time[0])
+print(sampling_frequency)
 # Perform FFT on the phase shift data
 fft_phase_shift = np.fft.fft(phase_shift)
 
@@ -46,10 +44,13 @@ positive_frequencies = frequencies[:len(frequencies) // 2]
 psd_positive = psd[:len(frequencies) // 2]
 
 # Plot the Power Spectral Density
+
 plt.figure(figsize=(10, 6))
-plt.semilogy(positive_frequencies, psd_positive)
-plt.title('Power Spectral Density of Phase Shift Signal')
-plt.xlabel('Frequency (Hz)')
-plt.grid(True)
+plt.semilogy(positive_frequencies / 10000, psd_positive)
+plt.title("Power Spectral Density of Phase Shift Signal")
+plt.xlabel("Frequency (kHz)")
+plt.ylabel("Power Spectral Density")
+plt.grid(which="both", linestyle="--", linewidth=0.5)
+plt.tight_layout()
 plt.savefig('FFT_Heat-gun_channel1.png')
 plt.show()
